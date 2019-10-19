6e630d283f3b5346048e2601393bd74c15dc09e8
Voyager <chriscoonwilliam@gmail.com>
2019-10-19 11:24:09 -0700

    finished js by making a inquiry function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -90,3 +90,83 @@ var trivia = {
     
   },
 
+  timerRunning : function(){
+
+    if(trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.gameQuestions).length){
+      $('#timer').text(trivia.timer);
+      trivia.timer--;
+        if(trivia.timer === 4){
+          $('#timer').addClass('last-seconds');
+        }
+    }
+
+    else if(trivia.timer === -1){
+      trivia.unanswered++;
+      trivia.result = false;
+      clearInterval(trivia.timerId);
+      resultId = setTimeout(trivia.guessResult, 1000);
+      $('#results').html('<h3>Out of time! The answer was '+ Object.values(trivia.answers)[trivia.currentSet] +'</h3>');
+    }
+
+    else if(trivia.currentSet === Object.keys(trivia.gameQuestions).length){
+      
+      $('#results')
+        .html('<h3>Great Job True Believer! Thanks For Playing!</h3>'+
+        '<p>Correct: '+ trivia.correct +'</p>'+
+        '<p>Incorrect: '+ trivia.incorrect +'</p>'+
+        '<p>Left Blank: '+ trivia.unanswered +'</p>'+
+        '<p>Give it another shot!</p>');
+      
+
+      $('#game').hide();
+      
+
+      $('#start').show();
+    }
+    
+  },
+
+  guessInquiry : function() {
+    
+
+    var resultId;
+    
+  
+    var correctAnswer = Object.values(trivia.answers)[trivia.currentSet];
+    
+
+    if($(this).text() === correctAnswer){
+
+        $(this).addClass('btn-success').removeClass('btn-info');
+      
+      trivia.correct++;
+      clearInterval(trivia.timerId);
+      resultId = setTimeout(trivia.guessResult, 2000);
+      $('#results').html('<h3>Right on the money!</h3>');
+    }
+
+    else{
+
+      $(this).addClass('btn-danger').removeClass('btn-info');
+      
+      trivia.incorrect++;
+      clearInterval(trivia.timerId);
+      resultId = setTimeout(trivia.guessResult, 2000);
+      $('#results').html('<h3>Nice try, but the correct answer was: '+ correctAnswer +'</h3>');
+    }
+    
+  },
+  
+  guessResult : function(){
+    
+
+    trivia.currentSet++;
+    
+    $('.option').remove();
+    $('#results h3').remove();
+    
+    trivia.nextQuestion();
+     
+  }
+
+}
